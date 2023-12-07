e77aa38712011e45f98adf3b2731bc9ce2c8730b
ALEXANDER <msumba83@gmail.com>
2023-12-07 12:52:52 +0200

    short circuiting && , || operators

--- script.js
+++ script.js
@@ -52,38 +52,65 @@ const restaurant = {
   },
 };
 
+console.log('=== OR ===');
+// use ANY data typeof, return ANY data typeof, short-circuiting
+console.log(3 || 'Alex');
+console.log('' || 'Alex');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guest1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guest1);
+
+const guest2 = restaurant.numGuests || 10;
+console.log(guest2);
+
+console.log('=== AND ===');
+console.log(0 && 'Alex');
+console.log(1 && 'Alex');
+console.log('IRENE' && 99 && null && 'Alex');
+
+// Practical Example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushroom', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
+
 // 1 DESTRUCTURING
 //SPREAD, because is on RIGHT side of =
 const arr = [1, 2, ...[3, 4]];
 
 // REST, because is on LEFT side of =
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
 
-const [pizza, , risotto, ...otherFood] = [
-  ...restaurant.mainMenu,
-  ...restaurant.starterMenu,
-];
-console.log(pizza, risotto, otherFood);
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
 
 // Objects
-const { sat, ...weekDays } = restaurant.openingHours;
-console.log(weekDays);
+// const { sat, ...weekDays } = restaurant.openingHours;
+// console.log(weekDays);
 
 // 2 FUNCTIONS
-const add = function (...numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
-  console.log(sum);
-};
-add(2, 3);
-add(5, 3, 7, 2);
-add(8, 2, 5, 3, 2, 1, 4);
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+//   console.log(sum);
+// };
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(8, 2, 5, 3, 2, 1, 4);
 
-const anotherArray = [23, 5, 7];
-add(...anotherArray);
-restaurant.orderPizza('mushroom', 'onion', 'banana', 'spinach');
-restaurant.orderPizza('mushroom')
+// const anotherArray = [23, 5, 7];
+// add(...anotherArray);
+// restaurant.orderPizza('mushroom', 'onion', 'banana', 'spinach');
+// restaurant.orderPizza('mushroom')
 
 ////////////////////////////////////////////////
 // The spread operator (...)
